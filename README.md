881db265006977797be311147705a07faf05b61e
elliottventura <elliott.ventura@gmail.com>
2022-04-07 08:57:46 +0000

    Gitpod link + simplify for quarkus connection

---
+++ README.md
@@ -0,0 +1,6 @@
+[![Open in Gitpod](https://gitpod.io/button/open-in-gitpod.svg)](https://gitpod.io/projects/test-api-go)
+
+# Install dependencies
+go get .
+# Launch the application
+go run .
--- main.go
+++ main.go
@@ -6,10 +6,10 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type major struct {
-	Name    string `json:"name"`
-	Country string `json:"country"`
-}
+// type major struct {
+// 	Name    string `json:"name"`
+// 	Country string `json:"country"`
+// }
 
 // album represents data about a record album.
 type album struct {
@@ -17,13 +17,15 @@ type album struct {
 	Title  string  `json:"title"`
 	Artist string  `json:"artist"`
 	Price  float64 `json:"price"`
-	Major  major   `json:"major"`
+	// Major  major   `json:"major"`
 }
 
 // albums slice to seed record album data.
 var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99, Major: major{Name: "Sony", Country: "Japan"}},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99, Major: major{Name: "Universal", Country: "Nederlands"}},
+	// {ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99, Major: major{Name: "Sony", Country: "Japan"}},
+	// {ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99, Major: major{Name: "Universal", Country: "Nederlands"}},
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
 }
 
@@ -33,7 +35,7 @@ func main() {
 	router.GET("/albums/:id", getAlbumByID)
 	router.POST("/albums", postAlbums)
 
-	router.Run("localhost:8080")
+	router.Run("localhost:8082")
 }
 
 // getAlbums responds with the list of all albums as JSON.
